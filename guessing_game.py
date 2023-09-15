34baac0be2f6a9f9d1cb57b57f57ffe826d9b96a
Dino <dineshnarayan19@gmail.com>
2023-09-16 00:13:50 +0530

    just bored, so wrote this; used rich formatting

--- guessing_game.py
+++ guessing_game.py
@@ -1,26 +1,28 @@
 import random
+from rich import print
 
 # Generate a random number between 1 and 999
 secret_number = random.randint(1, 999)
 
 def print_box(message):
-    print("*" * (len(message) + 4))
-    print(f"* {message} *")
-    print("*" * (len(message) + 4))
+    styled_message = f"[bold cyan]{message}[/bold cyan]"
+    print(f"{'*' * (len(styled_message) + 4)}")
+    print(f"* {styled_message} *")
+    print(f"{'*' * (len(styled_message) + 4)}")
 
 # Game loop
 while True:
     try:
         guess = int(input("Guess the number (1-999): "))
     except ValueError:
-        print("Invalid input. Please enter a valid number.")
+        print("[bold red]Invalid input.[/bold red] Please enter a valid number.")
         continue
 
     if guess < secret_number:
-        print("The number is too small.")
+        print("[bold yellow]The number is too small.[/bold yellow]")
     elif guess > secret_number:
-        print("The number is too big.")
+        print("[bold yellow]The number is too big.[/bold yellow]")
     else:
-        print_box("You're an idiot!")
+        print_box("[bold green]You're an idiot![/bold green]")
         break
 
