94d4c61fab703d7adb0a10205a4a2878f01fdfd4
Dino <dineshnarayan19@gmail.com>
2023-09-25 14:33:09 +0530

    Perfected it

--- mindmapping2.py
+++ mindmapping2.py
@@ -1,48 +1,73 @@
 
 import typer
-from rich import print
+from rich.console import Console
+from rich.tree import Tree
+from rich.style import Style
 from colorama import init
 
 init(autoreset=True)
 
+class CustomTreeNode:
+    def __init__(self, text):
+        self.text = text
+        self.children = []
+
 class MindMap:
     def __init__(self):
-        self.nodes = {'Root': {'children': [], 'parent': None}}
+        self.nodes = {'Root': CustomTreeNode('Root')}
+        self.tree = Tree("Mind Map", style=Style(color="blue"))
 
     def add_node(self, parent, text):
         if parent not in self.nodes:
             typer.secho("Parent node not found.", fg=typer.colors.RED)
             return
-        new_node = {'children': [], 'parent': parent}
-        self.nodes[parent]['children'].append((text, new_node))
+        new_node = CustomTreeNode(text)
+        self.nodes[parent].children.append(new_node)
         self.nodes[text] = new_node
+        self.update_tree()
 
     def remove_node(self, text):
         if text not in self.nodes:
             typer.secho("Node not found.", fg=typer.colors.RED)
             return
-        parent = self.nodes[text]['parent']
-        self.nodes[parent]['children'] = [(child, child_node) for child, child_node in self.nodes[parent]['children'] if child != text]
+        parent = self.find_parent(text)
+        if parent:
+            parent.children = [child for child in parent.children if child.text != text]
         del self.nodes[text]
+        self.update_tree()
+
+    def find_parent(self, text):
+        for node in self.nodes.values():
+            for child in node.children:
+                if child.text == text:
+                    return node
+        return None
+
+    def update_tree(self):
+        self.tree = Tree("Mind Map", style=Style(color="blue"))
+        self.build_tree(self.tree, self.nodes['Root'])
+
+    def build_tree(self, tree_node, custom_node):
+        tree_node.text = custom_node.text
+        tree_node.style = Style(color="blue")
+        for child in custom_node.children:
+            child_tree = Tree(child.text, style=Style(color="green"))
+            tree_node.add(child_tree)
+            self.build_tree(child_tree, child)
 
     def display(self):
-        tree = self.create_tree('Root')
-        print(tree)
-
-    def create_tree(self, node, depth=0):
-        tree = f"[bold green]{'  ' * depth}{node}[/bold green]\n"
-        for child, child_node in self.nodes[node]['children']:
-            tree += self.create_tree(child, depth + 1)
-        return tree
+        console = Console()
+        console.print(self.tree)
 
     def export_to_markdown(self, filename):
         with open(filename, 'w') as f:
             self._export_node_to_markdown('Root', f)
 
     def _export_node_to_markdown(self, node, f, depth=0):
-        f.write("  " * depth + "- " + node + "\n")
-        for child, child_node in self.nodes[node]['children']:
-            self._export_node_to_markdown(child, f, depth + 1)
+        if node != 'Root':
+            f.write("  " * depth + "- " + node + "\n")
+        for child in self.nodes[node].children:
+            self._export_node_to_markdown(child.text, f, depth + 1)
 
 def main():
     mind_map = MindMap()
@@ -69,15 +94,15 @@ def main():
             mind_map.remove_node(text)
         elif choice == '3':
             child = typer.prompt("Enter the child node to move to: ")
-            if child in [child for child, _ in mind_map.nodes[current_node]['children']]:
+            if child in [child.text for child in mind_map.nodes[current_node].children]:
                 current_node = child
             else:
                 typer.secho("Invalid child node.", fg=typer.colors.RED)
                 typer.pause()
         elif choice == '4':
-            parent = mind_map.nodes[current_node]['parent']
-            if parent is not None:
-                current_node = parent
+            parent = mind_map.find_parent(current_node)
+            if parent:
+                current_node = parent.text
             else:
                 typer.secho("Already at the root node.", fg=typer.colors.YELLOW)
                 typer.pause()
